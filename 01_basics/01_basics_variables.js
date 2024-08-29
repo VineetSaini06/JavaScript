6da2b2bef46877714c20e193d9433c3ba0fc868e
Vineet_Saini06 <vineetsaini.programmer@gmail.com>
2024-08-29 13:37:15 +0000

    how to declare variables

---
+++ 01_basics/01_basics_variables.js
@@ -0,0 +1,14 @@
+let name="vineet";
+const age=18;
+var city="dehradun";
+
+const surname=10;
+console.log(surname);
+
+//age=20; //this will show a error because if you create a const variable you can't change the value
+name="saini";   //you can change value of let variable
+city="kashipur";
+
+console.log(name,age,city); //this is the first method to print value of console
+console.table([name,age,city]); //this is second method to print all value at a time
+
