7df3de5092966dd6de4684e1e9147f72904e6011
Vineet_Saini06 <vineetsaini.programmer@gmail.com>
2024-08-29 13:37:44 +0000

    what are datatypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+" use strict "; //treat all JS code as newer version
+
+//alert("vineet"); //this will show error, because use are using nodejs, not browser
+
+//In JS there are 8 types of datatypes
+/*
+Number
+string
+null =standalone value
+undefined= when you declare a variable but not gives the value to variable
+symbol
+Boolean
+bigInt
+
+object
+
+*/
+
+let idLoggedIn=true;//boolean
